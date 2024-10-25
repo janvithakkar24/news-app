2bd80ee760370b5ef4e5e357e81cfcc47972efe8
Janvi Thakkar <35539604+janvithakkar24@users.noreply.github.com>
2024-10-25 10:50:13 +0530

    Added amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "newsapp46244b9d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
